1d3124f8703dafee944d2e5d70e5f694863a9533
oaklandgit <muppethead@mac.com>
2023-10-29 14:51:07 -0700

    Flag for shoing phone numbers

--- Sources/Peeps.swift
+++ Sources/Peeps.swift
@@ -1,17 +1,40 @@
 import ArgumentParser
 import SwiftyContacts
 
+func getHumanReadableLabel(for label: String) -> String {
+    let labelDict: [String: String] = [
+        CNLabelPhoneNumberMain: "Main",
+        CNLabelPhoneNumberMobile: "Mobile",
+        CNLabelPhoneNumberiPhone: "iPhone",
+    ]
+    return labelDict[label] ?? "Other"
+}
+
 @main
 struct Peeps: ParsableCommand {
     @Argument(help: "Search for person to peep.")
     public var input: String
 
+    @Flag(name: .shortAndLong, help: "Include phone numbers")
+    public var phoneNumbers: Bool = false
+
+    @Flag(name: .shortAndLong, help: "Include email addresses")
+    public var emailAddresses: Bool = false
+
     public func run() throws {
         let contacts = try fetchContacts(matchingName: self.input)
         for (index, contact) in contacts.enumerated() {
             print("\(index + 1). \(contact.givenName) \(contact.familyName)")
-            print("\(contact.identifier)")
-            // print(contact)
+
+            if self.phoneNumbers {
+                for phone in contact.phoneNumbers {
+                    if let label = phone.label {
+                        let humanReadableLabel = getHumanReadableLabel(for: label)
+                        print("  \(humanReadableLabel): \(phone.value.stringValue)")
+                    }
+                }
+            }
+
         }
 
     }
