b067ade116eaf6439dd80c207d77c4a420d9ecdf
oaklandgit <muppethead@mac.com>
2023-10-29 14:11:42 -0700

    Made search into an argument and not an option.

--- Sources/Peeps.swift
+++ Sources/Peeps.swift
@@ -3,13 +3,15 @@ import SwiftyContacts
 
 @main
 struct Peeps: ParsableCommand {
-    @Option(help: "Search for person to peep.")
+    @Argument(help: "Search for person to peep.")
     public var input: String
 
     public func run() throws {
         let contacts = try fetchContacts(matchingName: self.input)
         for (index, contact) in contacts.enumerated() {
             print("\(index + 1). \(contact.givenName) \(contact.familyName)")
+            print("\(contact.identifier)")
+            // print(contact)
         }
 
     }
